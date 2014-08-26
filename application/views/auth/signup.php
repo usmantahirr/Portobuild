5cab63cb14ab46ebe3cb0b418be4307476eea4f8
Usman Tahir <ch.usman.tahir@gmail.com>
2014-08-26 13:23:38 +0500

    Gallery files added to Theme2 + Google MAPS API Integration added

--- application/views/auth/signup.php
+++ application/views/auth/signup.php
@@ -205,23 +205,80 @@
     </div>
     
 </body>
-<script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
+<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places"></script>
 <script>
 var map;
 function initialize() {
+  var markers = [];
+  var map = new google.maps.Map(document.getElementById('map-canvas'), {
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+
   var mapOptions = {
     zoom: 8,
     center: new google.maps.LatLng(-34.397, 150.644)
   };
+  
+  // Create the search box and link it to the UI element.
+  var input = /** @type {HTMLInputElement} */(
+      document.getElementById('current_location'));
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  var searchBox = new google.maps.places.SearchBox(
+    /** @type {HTMLInputElement} */(input));
+
+  // [START region_getplaces]
+  // Listen for the event fired when the user selects an item from the
+  // pick list. Retrieve the matching places for that item.
+  google.maps.event.addListener(searchBox, 'places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+    for (var i = 0, marker; marker = markers[i]; i++) {
+      marker.setMap(null);
+    }
+
+    // For each place, get the icon, place name, and location.
+    markers = [];
+    var bounds = new google.maps.LatLngBounds();
+    for (var i = 0, place; place = places[i]; i++) {
+      var image = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      var marker = new google.maps.Marker({
+        map: map,
+        icon: image,
+        title: place.name,
+        position: place.geometry.location
+      });
+
+      markers.push(marker);
+
+      bounds.extend(place.geometry.location);
+    }
+
+    map.fitBounds(bounds);
+  });
+  // [END region_getplaces]
+
+  
+  
   map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
 }
-
 google.maps.event.addDomListener(window, 'load', initialize);
-        jQuery(document).ready(function(){
-            // binds form submission and fields to the validation engine
-            jQuery("#signup-form").validationEngine();
-        });
+jQuery(document).ready(function(){
+	// binds form submission and fields to the validation engine
+	jQuery("#signup-form").validationEngine();
+});
 </script>
 
 
